f7efcf51b17a1d2464b8e435aa3377e8ae7d860e
Truly <trulyj.sof@gmail.com>
2020-02-15 16:57:46 -0500

    add user login functionality with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users
+  #devise_for :users, path_names: {
+  #  sign_in: 'login', sign_out: 'logout',
+  #  password: 'secret', confirmation: 'verification',
+  #  registration: 'register', edit: 'edit/profile'
+  #}
+
   get 'homepage/index'
-  
-  root 'homepage#index'
+
+  #resources :users
+
+  root to: "homepage#index"
+  #root 'homepage#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
