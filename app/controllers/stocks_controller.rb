3fa797f8a74186b1903f3a90b50002559cb55a9c
Truly <trulyj.sof@gmail.com>
2020-02-17 11:35:29 -0500

    add AlphaVantageRB gem

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,4 +1,8 @@
 class StocksController < ApplicationController
   def new
+    stock = Alphavantage::Stock.new symbol: "MSFT", key: ENV['AV_KEY']
+    stock_quote = stock.quote
+    puts stock_quote.symbol
+    puts stock_quote.open
   end
 end
