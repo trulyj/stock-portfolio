8cfa5af536f8be6ba9488fe20704d6866f816e6d
Truly <trulyj.sof@gmail.com>
2020-02-17 17:49:34 -0500

    implement text colors on individual stock pages

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -9,8 +9,12 @@ class StocksController < ApplicationController
 
   def show
     @stock = Stock.find(params[:id])
-    #av = Alphavantage::Stock.new symbol: @stock.symbol, key: ENV['AV_KEY']
-    #@avinfo = av.quote
+    av = Alphavantage::Stock.new symbol: @stock.symbol, key: ENV['AV_KEY']
+    @avinfo = av.quote
+    @totalval = (@avinfo.price).to_f * (@stock.quantity)
+    puts @avinfo.price
+    puts @stock.quantity
+    puts @totalval
   end
 
   def new
@@ -25,10 +29,10 @@ class StocksController < ApplicationController
     @user = current_user
     @stock = @user.stocks.new(stock_params)
     if @stock.save
-      #stk = Alphavantage::Stock.new symbol: @stock.symbol, key: ENV['AV_KEY']
-      #stk_quote = stk.quote
-      #@user.balance = @user.balance - stk_quote.price*@stock.quantity
-      @user.balance = @user.balance - 60
+      stk = Alphavantage::Stock.new symbol: @stock.symbol, key: ENV['AV_KEY']
+      stk_quote = stk.quote
+      @user.balance = @user.balance - stk_quote.price*@stock.quantity
+      #@user.balance = @user.balance - 60
       @user.save
       @user.transactions.create(buyorsell: "BUY", symbol: @stock.symbol, quantity: @stock.quantity, price: 60, time: DateTime.now)
       redirect_to @stock
@@ -47,3 +51,27 @@ class StocksController < ApplicationController
     params.require(:stock).permit(:symbol, :quantity)
   end
 end
+
+#####
+#<p>
+#  <strong>Current Price per Share:</strong>
+#  <% if @avinfo.price > @avinfo.open %>
+#    <font color="green"> <%= @avinfo.price %> </font>
+#  <% elsif @avinfo.price < @avinfo.open %>
+#    <font color="red"> <%= @avinfo.price %> </font>
+#  <% else %>
+#    <font color="gray"> <%= @avinfo.price %> </font>
+#  <% end %>
+#</p>
+
+#<p>
+#  <strong>Current Total Value:</strong>
+#  <% if @avinfo.price > @avinfo.open %>
+#    <font color="green"> <%= @avinfo.price*@stock.quantity %> </font>
+#  <% else if @avinfo.price < @avinfo.open %>
+#    <font color="red"> <%= @avinfo.price*@stock.quantity %> </font>
+#  <% else %>
+#    <font color="gray"> <%= @avinfo.price*@stock.quantity %> </font>
+#  <% end %>
+#</p>
+#
