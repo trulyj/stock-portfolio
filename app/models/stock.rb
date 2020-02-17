ad073716cac88d99ef4e4cf340f4229d2bc56197
Truly <trulyj.sof@gmail.com>
2020-02-17 14:06:50 -0500

    remove unecessary puts statements

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,5 @@
 class SymbolValidator < ActiveModel::Validator
   def validate(stock)
-    puts "symbol validate begin"
     begin
       stk = Alphavantage::Stock.new symbol: stock.symbol, key: ENV['AV_KEY']
       puts stk
@@ -8,12 +7,10 @@ class SymbolValidator < ActiveModel::Validator
       stock.errors[:base] << "Stock symbol is invalid."
       #puts stock.errors[:base]
     end
-    puts "symbol validate end"
   end
 end
 
 class Stock < ApplicationRecord
   validates :quantity, presence: true, numericality: { greater_than: 0 }
-  puts "quantity validated"
   validates_with SymbolValidator
 end
